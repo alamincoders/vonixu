50b059430ab0debb7f7884a7bd690885f6398056
Your Name <you@example.com>
2022-10-24 22:53:11 +0600

    about page

--- components/AboutVonixu.jsx
+++ components/AboutVonixu.jsx
@@ -70,7 +70,6 @@ const AboutVonixu = () => {
         </div>
       </div>
 
-
       {/* Our Values */}
       <div className="container py-10 ">
         <h1 className="text-4xl font-bold">
@@ -83,9 +82,7 @@ const AboutVonixu = () => {
           decision we make, every strategy we use, every partnership we
           entertain is based on these core values.
         </p>
-
         <div class="space-y-10 mt-10 grid md:grid-cols-2 ">
-
           <details class="group border-l-[12px] border-cyan-500 bg-gray-50 p-6 rounded-b-lg rounded-t-lg ">
             <summary class="flex cursor-pointer items-center justify-between ">
               <h2 class="text-lg font-medium text-gray-900">
@@ -142,7 +139,6 @@ const AboutVonixu = () => {
               consequuntur distinctio corporis earum similique!
             </p>
           </details>
-
         </div>
       </div>
 
@@ -667,6 +663,179 @@ const AboutVonixu = () => {
         </div>
       </section>
 
+      {/* Testimonial  */}
+      <div className="container py-10">
+        <h1 className="text-4xl font-bold">
+          <span className="text-primary">Testimonials From Our</span>{" "}
+          <span className="text-secondary ">Clients</span>
+        </h1>
+
+        <div className="w-[240px] h-1 bg-primary border-0 rounded-2xl "></div>
+
+        <section class="text-gray-600 body-font">
+          <div class="pt-14 mx-auto">
+            <div class="flex flex-wrap -m-4">
+              <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                <div class="h-full text-center">
+                  <img
+                    alt="testimonial"
+                    class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://dummyimage.com/302x302"
+                  />
+                  <p class="leading-relaxed">
+                    Edison bulb retro cloud bread echo park, helvetica stumptown
+                    taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin
+                    coffee ennui shaman taiyaki vape DIY tote bag drinking
+                    vinegar cronut adaptogen squid fanny pack vaporware.
+                  </p>
+                  <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                  <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    HOLDEN CAULFIELD
+                  </h2>
+                  <p class="text-gray-500">Senior Product Designer</p>
+                </div>
+              </div>
+              <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                <div class="h-full text-center">
+                  <img
+                    alt="testimonial"
+                    class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://dummyimage.com/300x300"
+                  />
+                  <p class="leading-relaxed">
+                    Edison bulb retro cloud bread echo park, helvetica stumptown
+                    taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin
+                    coffee ennui shaman taiyaki vape DIY tote bag drinking
+                    vinegar cronut adaptogen squid fanny pack vaporware.
+                  </p>
+                  <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                  <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    ALPER KAMU
+                  </h2>
+                  <p class="text-gray-500">UI Develeoper</p>
+                </div>
+              </div>
+              <div class="lg:w-1/3 lg:mb-0 p-4">
+                <div class="h-full text-center">
+                  <img
+                    alt="testimonial"
+                    class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://dummyimage.com/305x305"
+                  />
+                  <p class="leading-relaxed">
+                    Edison bulb retro cloud bread echo park, helvetica stumptown
+                    taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin
+                    coffee ennui shaman taiyaki vape DIY tote bag drinking
+                    vinegar cronut adaptogen squid fanny pack vaporware.
+                  </p>
+                  <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                  <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    HENRY LETHAM
+                  </h2>
+                  <p class="text-gray-500">CTO</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* Get In Touch */}
+      <div className="container py-10">
+        <h1 className="text-4xl font-bold">
+          <span className="text-primary">Get In</span>{" "}
+          <span className="text-secondary ">Touch</span>
+        </h1>
+
+        <div className="w-[108px] h-1 bg-primary border-0 rounded-2xl "></div>
+
+        <div class=" pt-14 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe
+              width="100%"
+              height="100%"
+              class="absolute inset-0"
+              frameborder="0"
+              title="map"
+              marginheight="0"
+              marginwidth="0"
+              scrolling="no"
+              src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+            ></iframe>
+            <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+              <div class="lg:w-1/2 px-6">
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p class="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
+              </div>
+              <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a class="text-indigo-500 leading-relaxed">example@email.com</a>
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p class="leading-relaxed">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+              Contact Me
+            </h2>
+            <p class="leading-relaxed mb-5 text-gray-600">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="message" class="leading-7 text-sm text-gray-600">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+
+            <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Message
+            </button>
+
+            <p class="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
