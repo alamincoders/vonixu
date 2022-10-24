2b251a4fd16f3f315d7e9d6544b8055f643209e4
Your Name <you@example.com>
2022-10-24 21:39:51 +0600

    about vonixu

---
+++ components/AboutVonixu.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const AboutVonixu = () => {
+  return (
+    <section className="container py-1">
+      <h1 className="text-4xl font-bold">
+        <span className="text-primary">About</span>{" "}
+        <span className="text-secondary ">Vonixu</span>
+      </h1>
+
+      <div className="w-[108px] h-1 bg-primary border-0 rounded-2xl "></div>
+
+      <p className='text-[20px] mt-10 font-sans ' >
+        Bizcope provides online marketing campaigns across the key areas of
+        search engine marketing, affiliate marketing and advertising that help
+        companies grow revenue. We started our official journey in 2010 under
+        the name Outsource BD and began with a focus on small online business.
+        After three successive business years in 2013, we rebranded to become
+        Bizcope and evolved as a large online marketing firm. Now, we at
+        Bizcope, provides various internet marketing services for small and
+        medium-sized business to enterprise brands across the globe with a great
+        rate of success.
+      </p>
+
+      <div className=' mt-16 flex-none md:flex items-center text-left gap-x-8' >
+        <div className=' w-50 shadow-xl rounded-xl p-10  ' >
+          <h1 className=' text-primary font-bold text-2xl ' >We Are Bizcopers</h1>
+          <p className=' mt-6 text-[17px]  ' >
+            We are the people who eat, drink, sleep and play with SE0 and
+            development. We don’t do work for money, we love this work and
+            that’s why we earn money. We love to work collaboratively with our
+            clients to ensure that the objectives are met by our proven
+            strategies.
+          </p>
+        </div>
+        <div className=' w-50 shadow-xl rounded-xl p-10  ' >
+          <h1 className=' text-primary font-bold text-2xl ' >How We Formed</h1>
+          <p className=' mt-6 text-[17px]' >
+            When we started we had nothing more than few computers, now have 18
+            members in our team. How we did it? We formed with very easy
+            calculation, gradually with 1-18 by satisfying over 300
+            clients for SEO, SMM and Development Projects for the last six.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutVonixu;
