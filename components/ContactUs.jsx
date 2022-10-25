e666378461e9e57923066392d01fd9f138eab8d0
Your Name <you@example.com>
2022-10-25 13:39:13 +0600

    blog section half done

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -1,10 +1,40 @@
-import Button from "./shared/Button";
+import React from 'react';
+import { MeetUsData } from '../data/data';
+import Button from './shared/Button';
 
-const ContactMe = () => {
+const ContactUs = () => {
   return (
-      <section className="text-gray-600 bg-gray-50 mt-20 rounded-2xl container  py-10 body-font relative">
+    <div>
+      {/* Meet Us */}
+      <div className='container py-10' >
         <h1 className="text-4xl font-bold">
-          <span className="text-primary">Get In</span> <span className="text-secondary ">Touch</span>
+          <span className="text-primary">This Happens When You</span>{" "}
+          <span className="text-secondary ">Meet Us​</span>
+        </h1>
+
+        <div className="w-[108px] h-1 bg-primary border-0 rounded-2xl "></div>
+
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-10 py-10">
+          {MeetUsData.map((meetUs) => (
+            <div key={meetUs.id}>
+              <div className="relative block cursor-pointer hover:bg-gray-200 hover:rounded-lg duration-300 transition ease-in-out border-violet-600 rounded-tl-lg p-8 shadow-md ">
+                <h3 className="text-2xl text-primary font-bold">
+                  {meetUs.name}
+                </h3>
+                <p className="mt-4 text-[16px] font-medium text-gray-700">
+                  {meetUs.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Get In Touch */}
+      <div className="text-gray-600 bg-gray-50 mt-20 rounded-2xl container  py-10 body-font relative">
+        <h1 className="text-4xl font-bold">
+          <span className="text-primary">Get In</span>{" "}
+          <span className="text-secondary ">Touch</span>
         </h1>
 
         <div className="w-[108px] h-1 bg-primary border-0 rounded-2xl "></div>
@@ -24,21 +54,36 @@ const ContactMe = () => {
             ></iframe>
             <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
               <div className="lg:w-1/2 px-6">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p className="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
               </div>
               <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                <a className="text-indigo-500 leading-relaxed">example@email.com</a>
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a className="text-indigo-500 leading-relaxed">
+                  example@email.com
+                </a>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
                 <p className="leading-relaxed">123-456-7890</p>
               </div>
             </div>
           </div>
 
           <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-gray-900 mb-1  title-font text-3xl font-semibold  ">Contact Us</h2>
-            <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+            <h2 className="text-gray-900 mb-1  title-font text-3xl font-semibold  ">
+              Contact Us
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
             <div className="relative mb-4">
               <label for="name" className="leading-7 text-sm text-gray-600">
                 Name
@@ -74,11 +119,15 @@ const ContactMe = () => {
 
             <Button title="Message" />
 
-            <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+            <p className="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
   );
 };
 
-export default ContactMe;
+export default ContactUs;
--- data/data.js
+++ data/data.js
@@ -55,3 +55,39 @@ export const OurServicesData = [
     description: 'Digital marketing creates opportunities for small and medium business enterprises to compete with larger business entities.'
   }
 ]
+
+
+// Meet Us​ Data
+
+export const MeetUsData = [
+  {
+    name: "Building Strength",
+    description:
+      "We together build a strong relationship, which will increase the strength of our work.",
+  },
+  {
+    name: "Learning On the Go",
+    description:
+      "It’s not only a working phase also we will learn and experience all the things together.",
+  },
+  {
+    name: "Fixing the Mindset",
+    description:
+      "We will fix a mindset to achieve our goal in time. We work for your dream to make it real.",
+  },
+  {
+    name: "Improve Your Ranking",
+    description:
+      "Improving your ranking is our prior task if you choose to take our SEO service. Let us deal with Google for you.",
+  },
+  {
+    name: "Positive Result",
+    description:
+      "Together we will achieve a positive result to set your business as a brand in the market. Always Positive Thinking",
+  },
+  {
+    name: "Great Heart Pace",
+    description:
+      "Your website is the heart of your business. We work just like a pacemaker to keep your heart healthy.",
+  },
+];
