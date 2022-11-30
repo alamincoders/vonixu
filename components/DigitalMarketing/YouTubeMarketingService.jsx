4fd086876b60b0efb43d8e00d840217de076eb1a
Your Name <you@example.com>
2022-11-30 10:04:42 +0600

    YouTubeMarketingService content done

--- components/DigitalMarketing/YouTubeMarketingService.jsx
+++ components/DigitalMarketing/YouTubeMarketingService.jsx
@@ -6,19 +6,27 @@ import WhatSEO from "../ServicesComponents/WhatSEO";
 import SEOBenefits from "../ServicesComponents/SEOBenefits";
 
 import seo from "../../assets/services/whatseo.png";
-
-import bImg from "../../assets/services/local.webp";
 import Banner from "../ServicesComponents/Banner";
 import ServiceFAQ from "../ServicesComponents/ServiceFAQ";
 
 const YouTubeMarketingService = () => {
   return (
     <>
-      <Banner title="VISER SEO Services: Rank on Search Engine & Drive Organic Traffic to Your Website" />
+      <Banner title="Best YouTube Marketing Service: Reach People & Grow Revenue" />
       <SeoFormBanner
-        title="Affordable Local SEO"
-        p="Manta has ranked millions of local businesses in the top 10 of Google for over 20 years with our affordable local SEO services. We help businesses get found by a large local audience to increase both leads and sales. Learn how our local  SEO services can help you find new customers today!"
-        img={bImg}
+        title="Generate More Revenue From the Best YouTube Marketing Services at Vonixu "
+        p1="Video marketing is becoming more familiar nowadays as people are watching videos more frequently than before.
+
+Many small businesses are getting the desired revenue by targeting potential people interested in your products or services.
+"
+        p2="You should know that YouTube has the most potential customers in terms of branding videos and generating sales massively.
+
+Vonixu  has the most sophisticated YouTube marketing strategy for businesses with enough research on this platform.
+"
+        p3="Our experts are working on YouTube marketing for more than a decade to our clients and generate profit.
+
+We are not only optimizing channels but also adding in-depth descriptions to each video on your channel so that it can rank higher on YouTube as well as google search.
+"
       />
       <OverPercent
         title1="Over 50%"
@@ -30,10 +38,18 @@ const YouTubeMarketingService = () => {
       />
       <WhatSEO
         img={seo}
-        title="What is Affordable Local SEO?"
-        p1="Local SEO, also referred to as local search, is a marketing strategy aimed to get your website seen in the top 10 organic positions on Google  when customers near you search for your products online."
-        p2=" Like traditional SEO, it focuses on increasing search engine rankings, but focuses on local keywords. There are two places to do this—Local Map Pack and Google SERPs (search engine results page)."
-        p3=" Local SEO is essential for achieving strong brand awareness in local communities. It can help businesses find potential customers and increase their book of business."
+        p1="If you are serious about achieving targeted profit from YouTube, then invest in it wisely.
+
+Otherwise, your short-term planning may end up a failure, and your business may get a wrong impression on your clients.
+"
+        p2=" We will solve your problem by implementing all ranking factors needed for ranking on YouTube and google search engine.
+
+Just make a call to the given number or email us using the form below. Our YouTube marketing executive will reach you with a schedule for your appointment.
+"
+        p3=" You will know how you can improve your marketing strategy with YouTube video marketing and generate profit and meet your ROI target.
+
+Please do not wait to contact us as soon as possible and make as much profit as you want from our YouTube marketing services.
+"
       />
       <SEOBenefits
         title1="Affordable Local SEO Services Benefits "
@@ -45,30 +61,33 @@ const YouTubeMarketingService = () => {
         p3=" Local search optimization focuses on increasing brand awareness and reputation through directory and citation building, positive online  review, and much more."
       />{" "}
       <ServiceFAQ
-        que1=""
-        que2=""
-        que3=""
-        que4=""
-        que5=""
-        que6=""
-        que7=""
-        que8=""
-        que9=""
-        que10=""
-        que11=""
-        que12=""
-        ans1=""
-        ans2=""
-        ans3=""
-        ans4=""
-        ans5=""
-        ans6=""
-        ans7=""
-        ans8=""
-        ans9=""
-        ans10=""
-        ans11=""
-        ans12=""
+        title="FAQ: Best YouTube Marketing Service"
+        p="Business owners are trying to understand how youtube marketing can bring potential visitors to their website through video advertisements. We are getting several questions from people sending us to our email and other media.
+
+To make the process easier for them, we have developed a dedicated section to answer all of their queries. Check out our Q&A section below to find your answers.
+"
+        que1="What is the YouTube Marketing Service?"
+        que2="How Do YouTube Advertisements Work?"
+        que3="How Much Does It Cost to Promote Videos on YouTube?"
+        que4="Will You Optimize My YouTube Channel?"
+        que5="Does Your Service Bring Organic View & Subscribers to My Channel?"
+        que6="Will I Get Enough Views on Videos of My Channel?"
+        que7="Do I Have to Give Creative Elements to Make Video Ads?"
+        que8="Can I See Your Work During My Project is Running?"
+        que9="Do You Give Me Any Progress Report?"
+        que10="Why Should I Rely on Vonixu?"
+        ans1="Our YouTube marketing service includes video ads creation, running ads on targeted YouTube videos. YouTube marketing service is a sophisticated marketing strategy to reach people while watching their favorite videos on YouTube. Youtube is the second largest search engine where potential customers are trying to get information about products or services they will probably buy soon."
+        ans2="Whenever you watch videos, you will find that automatically a video starts playing. After a few seconds, you have to click on skip ads to continue watching the video you were watching. Youtube has a sophisticated ad network system where you can target where your video ad will play."
+        ans3="YouTube has its payment process for showing ads like other ad networks like AdWords. The price is based on the keyword. On YouTube, you can start your marketing campaign with $10, and you can spend $0.10 to $0.30 for each view your video ad gets. You can place clickable assets on your ad that people can click if they find your ads interesting to them."
+        ans4="Yes, our service ensures the complete optimization of your youtube channel. We have in-house youtube optimization experts who will dedicatedly work on your projects. The optimization we provide at vonixu is unique and proven that no other agencies can bring to your channel."
+        ans5="Yes, our service will surely bring an organic view from organic searches that people are making on the youtube search engine. Our optimization will not only help your videos to rank on youtube to get enough views and subscribers but also send visitors to your business website, products, or service pages. We have top-notch writers who can write high-quality descriptions for each of your videos on your channel that also ranks on google for relevant keywords."
+        ans6="Yes, you will get enough views on videos of your channel through our optimization. You should know that to rank on youtube, you must do keyword research for your youtube channel. Our keyword research team has extensive experience in doing keyword research dedicatedly for youtube. They will find out the low-competitive niche relevant keywords that match your products or services.
+"
+        ans7="It depends on the packages you choose, but do not worry, and we have both options available. If you want to give creatives that are needed to create your videos or ads, then you can provide it. On the other hand, if you do not wish to hassle creating those, we can make them from our graphics design department. Our graphic experts are experienced in professional video"
+        ans8="Of course, without any hesitation. Just ask us what you want to know. We will assign a dedicated marketing manager for your project who will provide you updates. Whenever you want to know anything, ask your manager, he will do it immediately."
+        ans9="Well, as youtube marketing is a long-term project that you should continue if you want serious growth of revenue to your business. We will give you a monthly report to show you how your youtube marketing campaign is running. After reading the report, you can recommend us if you have anything you want us to change."
+        ans10="We are working on youtube after it started. We have enough resources as well as proven strategies and working plans that we are implementing on our clients and projects. If you choose us to work on your project, then you will get the result within the shortest time possible. You can rely on vonixu because we have served thousands of clients and earned millions of dollars as revenue by increasing sales and collecting leads.
+"
       />
     </>
   );
